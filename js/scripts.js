e88a4ed68fff6eb85fbb10ecd1d34aa7f79bf639
Alesandria Wild <alesandriawild@yahoo.com>
2022-08-08 15:07:57 -0700

    Add simplified window alert to only show products and sums on a single alert box.
    Co-authored-by: Spencer Dennis <sjdennis2113@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -5,16 +5,12 @@ function add(number1, number2) {
 const number1 = parseInt(prompt("Enter a number:"));
 const number2 = parseInt(prompt("Enter another number:"));
 
-window.alert("The sum of your number is " + add(number1, number2) + ".");
-
 function subtract(number3, number4) {
   return number3 - number4;
 }
 
 const number3 = parseInt(prompt("Enter a number:"));
 const number4 = parseInt(prompt("Enter another number:"));
-
-window.alert("The sum of your number is " + subtract(number3, number4) + ".");
 
 function divide(number5, number6) {
   return number5 / number6;
@@ -23,8 +19,6 @@ function divide(number5, number6) {
 const number5 = parseInt(prompt("Enter a number:"));
 const number6 = parseInt(prompt("Enter another number:"));
 
-window.alert("The product of your number is " + divide(number5, number6) + ".");
-
 function multiply(number7, number8) {
   return number7 * number8;
 }
@@ -32,4 +26,4 @@ function multiply(number7, number8) {
 const number7 = parseInt(prompt("Enter a number:"));
 const number8 = parseInt(prompt("Enter another number:"));
 
-window.alert("The product of your number is " + multiply(number7, number8) + ".");
+window.alert("The additon of your numbers equals " + add(number1, number2) + ". " + "The subtraction of your numbers equals " + subtract(number3, number4) + ". " + "The product of your number is " + divide(number5, number6) + ". " + "The product of your number is " + multiply(number7, number8) + ".");
