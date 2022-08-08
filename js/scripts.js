39854ea2c24a196d1cd989989673f567d15539ba
Alesandria Wild <alesandriawild@yahoo.com>
2022-08-08 14:33:54 -0700

    Add sub, mult, and div functions completed. Code functioning as written.
    Co-authored-by: Spencer Dennis <sjdennis2113@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -7,7 +7,29 @@ const number2 = parseInt(prompt("Enter another number:"));
 
 window.alert(add(number1, number2));
 
-function subtract(number1, number2) {
-  return number1 - number2;
+function subtract(number3, number4) {
+  return number3 - number4;
 }
 
+const number3 = parseInt(prompt("Enter a number:"));
+const number4 = parseInt(prompt("Enter another number:"));
+
+window.alert(subtract(number3, number4));
+
+function divide(number5, number6) {
+  return number5 / number6;
+}
+
+const number5 = parseInt(prompt("Enter a number:"));
+const number6 = parseInt(prompt("Enter another number:"));
+
+window.alert(divide(number5, number6));
+
+function multiply(number7, number8) {
+  return number7 * number8;
+}
+
+const number7 = parseInt(prompt("Enter a number:"));
+const number8 = parseInt(prompt("Enter another number:"));
+
+window.alert(multiply(number7, number8));
