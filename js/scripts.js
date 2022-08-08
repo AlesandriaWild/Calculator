ebf54915db558c41170d431bfa9377c6ce1feaaf
Alesandria Wild <alesandriawild@yahoo.com>
2022-08-08 14:47:25 -0700

    All basic math functions are working. Checked multiple times, and checked in the dev tool.
    Co-authored-by: Spencer Dennis <sjdennis2113@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -5,7 +5,7 @@ function add(number1, number2) {
 const number1 = parseInt(prompt("Enter a number:"));
 const number2 = parseInt(prompt("Enter another number:"));
 
-window.alert(add(number1, number2));
+window.alert("The sum of your number is " + add(number1, number2) + ".");
 
 function subtract(number3, number4) {
   return number3 - number4;
@@ -14,7 +14,7 @@ function subtract(number3, number4) {
 const number3 = parseInt(prompt("Enter a number:"));
 const number4 = parseInt(prompt("Enter another number:"));
 
-window.alert(subtract(number3, number4));
+window.alert("The sum of your number is " + subtract(number3, number4) + ".");
 
 function divide(number5, number6) {
   return number5 / number6;
@@ -23,7 +23,7 @@ function divide(number5, number6) {
 const number5 = parseInt(prompt("Enter a number:"));
 const number6 = parseInt(prompt("Enter another number:"));
 
-window.alert(divide(number5, number6));
+window.alert("The product of your number is " + divide(number5, number6) + ".");
 
 function multiply(number7, number8) {
   return number7 * number8;
@@ -32,4 +32,4 @@ function multiply(number7, number8) {
 const number7 = parseInt(prompt("Enter a number:"));
 const number8 = parseInt(prompt("Enter another number:"));
 
-window.alert(multiply(number7, number8));
+window.alert("The product of your number is " + multiply(number7, number8) + ".");
