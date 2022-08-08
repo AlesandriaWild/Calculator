b29c878ed047ca237a65a7f98e53b3c112365349
Alesandria Wild <alesandriawild@yahoo.com>
2022-08-08 16:19:32 -0700

    Fixed bug, displaying all functions in one window alert currently.
    Co-authored-by: Spencer Dennis <sjdennis2113@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,31 +1,25 @@
 function add(number1, number2) {
   return number1 + number2;
 }
- 
+
+function subtract(number1, number2) {
+  return number1 - number2;
+}
+
+function divide(number1, number2) {
+  return number1 / number2;
+}
+
+function multiply(number1, number2) {
+  return number1 * number2;
+}
+
 const number1 = parseInt(prompt("Enter a number:"));
 const number2 = parseInt(prompt("Enter another number:"));
 
-function subtract(number3, number4) {
-  return number3 - number4;
-}
+const additon = number1.toString() + " + " + number2.toString() + " = " + add(number1, number2).toString();
+const subtraction = number1.toString() + " - " + number2.toString() + " = " + subtract(number1, number2).toString();
+const division = number1.toString() + " / " + number2.toString() + " = " + divide(number1, number2).toString();
+const multiplication = number1.toString() + " * " + number2.toString() + " = " + multiply(number1, number2).toString();
 
-const number3 = parseInt(prompt("Enter a number:"));
-const number4 = parseInt(prompt("Enter another number:"));
-
-function divide(number5, number6) {
-  return number5 / number6;
-}
-
-const number5 = parseInt(prompt("Enter a number:"));
-const number6 = parseInt(prompt("Enter another number:"));
-
-function multiply(number7, number8) {
-  return number7 * number8;
-}
-
-const number7 = parseInt(prompt("Enter a number:"));
-const number8 = parseInt(prompt("Enter another number:"));
-
-console.log(add(number1,number2))
-
-window.alert("The additon of your numbers equals " + "" + add(number1, number2) + "" + ". " + "The subtraction of your numbers equals " + subtract(number3, number4) + ". " + "The product of your number is " + divide(number5, number6) + ". " + "The product of your number is " + multiply(number7, number8) + ".");
+window.alert (additon + "\n" + subtraction + "\n" + division + "\n" + multiplication);
