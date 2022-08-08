2a485262a86f805d7fc78805c7918b4037a13182
Alesandria Wild <alesandriawild@yahoo.com>
2022-08-08 14:21:48 -0700

    Add README
    Co-authored-by: Spencer Dennis <sjdennis2113@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -6,3 +6,8 @@ const number1 = parseInt(prompt("Enter a number:"));
 const number2 = parseInt(prompt("Enter another number:"));
 
 window.alert(add(number1, number2));
+
+function subtract(number1, number2) {
+  return number1 - number2;
+}
+
